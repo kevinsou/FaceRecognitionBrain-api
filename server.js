1043b0769e93197b2ad4b10e8d0e29cb0c4bedc4
kevinsou <kevinsou.ks@gmail.com>
2019-02-09 00:01:58 -0800

    fixing database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-concave-32433',
-    user : 'postgres',
-    password : 'kevinsou',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
